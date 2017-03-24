ca19258e33e13043a4701c04e7ad5dfb7a905af7
Wendel <wendelrm.bg@gmail.com>
2017-03-24 10:54:20 -0300

    Corrige leitura do valor do campo
    Só lia o valor até o tamanho da tag subtraindo 1
    Ex: <ACCTID>000000525
    - Neste caso ele só pegava 7 caracteres: 0000005

--- source/ofxreader.pas
+++ source/ofxreader.pas
@@ -220,7 +220,7 @@ begin
     if pos('</', sLine) > 0 then
        Result := copy(sLine, iTemp+1, pos('</', sLine)-iTemp-1)
     else
-       Result := copy(sLine, iTemp+1, iTemp-1);
+       Result := copy(sLine, iTemp+1, Length(sLine));
   end;
 end;
 
